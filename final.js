742572c3abf0f5960b88f483dea9b715553d271d
Fidel Lascano <fidel.lascano@conauto.com.ec>
2023-05-17 17:12:24 -0500

    Add searh bar

--- final.js
+++ final.js
@@ -1,1 +1,16 @@
-document.addEventListener("DOMContentLoaded", function(event) {});
+const toggleIcon = (element, [texX, textY]) => {
+    element.textContent = element.textContent === texX ? textY : texX;
+};
+document.addEventListener("DOMContentLoaded", function(event) {
+    //Agregando event de busqueda
+    const btnSearch = document.querySelector(".btn-search");
+    btnSearch.addEventListener("click", function(event) {
+        event.preventDefault();
+        const currenText = event.target.textContent;
+        toggleIcon(event.target, ["close", "search"])
+       const search = document.querySelector(".nav-section-search");
+       const hideShowClass = currenText === 'close' ? "hide" : "show";
+       search.setAttribute("class", `nav-section-search ${hideShowClass}`)
+    });
+
+});
