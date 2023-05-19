78ef5fbd0dcdb11bcb2bb966281db53d9df6f37c
Fidel Lascano <fidel.lascano@conauto.com.ec>
2023-05-19 16:48:29 -0500

    add shopt page

--- final.js
+++ final.js
@@ -1,17 +1,74 @@
 const productos = [
-    { id: 1, img:'https://polipapel.vteximg.com.br/arquivos/ids/158002-1000-1000/lapices-Y00836.jpg?v=637227641003330000', description: 'Lapices Craft', details: 'detalles', price:  30, stock: 10},
-    { id: 2, img:'', description: 'Lapices Faber', details: 'detalles', price:  12, stock: 20}
+    {
+        id: 1,
+        img: 'https://polipapel.vteximg.com.br/arquivos/ids/158002-1000-1000/lapices-Y00836.jpg?v=637227641003330000',
+        description: 'Lapices Craft',
+        details: 'detalles',
+        price: 30,
+        stock: 10,
+        category: 'lapices'
+    },
+    {
+        id: 2,
+        img: 'https://juanmarcet.com/wp-content/uploads/2021/01/0201132470.jpg',
+        description: 'Lapices Faber',
+        details: 'detalles',
+        price: 12,
+        stock: 20,
+        category: 'lapices'
+    },
+    {
+        id: 3,
+        img: 'https://juanmarcet.com/wp-content/uploads/2021/04/0201118248.jpg',
+        description: 'Cuaderno De Dibujo Espiral',
+        details: 'detalles',
+        price: 7.75,
+        stock: 80,
+        category: 'cuadernos'
+    },
+    {
+        id: 4,
+        img: 'https://juanmarcet.com/wp-content/uploads/2022/09/00210326-1.jpg',
+        description: 'Cuaderno Grapado Escribe Dibujo',
+        details: 'detalles',
+        price: 0.66,
+        stock: 80,
+        category: 'cuadernos'
+    }
 ]
+
+
+const cart = [];
+
+const fnAddToCart = (event, id) => {
+    const product = productos.find((product) => product.id === id);
+    const productInCart = cart.find((product) => product.id === id);
+    const stock = document.querySelector(`#stock-${id}`);
+    if (product.stock > 0) {
+        product.stock--;
+        if (productInCart) {productInCart.stock++;}
+        else {cart.push({...product, stock:1});}
+        stock.textContent = product.stock;
+        console.log(cart);
+    }
+};
 
 const createProduct = (product) =>
     (` <div class="card">
                 <img src="${product.img}" class="card-img-top" alt="..."> 
                 <div class="card-body">
-                <p class="card-text">${product.description}</p>
-                <p class="card-text">${product.details}</p>
-                <p class="card-text">${product.price}</p>
-                <p class="card-text">${product.stock}</p>
-                <button class="btn btn-primary">Comprar</button>
+                <p class="card-text card-description">
+                    <span class="card-item-title"></span>
+                    <span class="card-item-value">${product.description}</span>
+                </p>
+                <p class="card-text card-price">
+                    ${product.price}
+                </p>
+                <p class="card-text card-stock">
+                    <span class="card-item-title">Stock:</span>
+                    <span class="card-item-value" id="stock-${product.id}">${product.stock}</span>
+                </p>
+                    <button class="btn btn-primary card-botton" onclick="fnAddToCart(event,${product.id})">Comprar</button>
                 </div>
             </div>`)
 
@@ -22,7 +79,6 @@ const renderProducts = (products, container) => {
         container.innerHTML += html;
     });
 }
-
 
 
 const toggleIcon = (element, [texX, textY]) => {
@@ -36,16 +92,15 @@ const router = (element, path) => {
         .then((response) => response.text())
         .then((html) => {
             element.innerHTML = html;
-            path === 'fragments/shop' && renderProducts(productos, container);
         }).finally(
-            () => {
-                switch (path) {
-                    case 'fragments/shop':{
-                        const container = document.querySelector('.products-container');
-                        renderProducts(productos, container);
-                    }
+        () => {
+            switch (path) {
+                case 'fragments/shop': {
+                    const container = document.querySelector('.products-container');
+                    renderProducts(productos, container);
                 }
             }
+        }
     );
 }
 
